4d523b374db50bdaf1ec3deba6bbdb75e5a9b83d
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2022-09-02 10:52:36 -0500

    Busqueda-Paginación
    100%

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -13,6 +13,7 @@ var p9 = new producto(9, "Aceite corporal blanco siberiano anticelulítico de Na
 var p10 = new producto(10, "Aceite corporal Body Sculptor", "", 73.70, 491, "Aceite corporal que moldea el cuerpo y esculpe la silueta de forma natural y eficaz. Previene el exceso de peso y la retención de líquidos gracias a su acción drenante, activa la microcirculación a la vez que tonifica la piel. Esculpe tu cuerpo realizando tratamientos de forma diaria. Está formulado con aceites vegetales naturales adecuado para pieles sensibles.", "../img/10.jpg")
 var p11 = new producto(11, "Aceite corporal de almendras dulces", "500ML", 10.45, 20.90, "El Aceite de Almendras dulces es básico para una hidratación y nutrición de la piel. Puedes utilizarlo en todas las partes de tu cuerpo preferiblemente después de la ducha con la piel húmeda, para ayudar a su absorción llegando a las capas profundas de la piel. Un aceite neutro apto para todo tipo de pieles y edades, utilizándose para toda la familia desde las edades más tempranas. Es ideal como base para formularlo con otros aceites, aceites esenciales y lociones. ", "../img/11.jpg")
 var p12 = new producto(12, "Aceite corporal de almendras dulces con dosificador 1L", "1000ML", 14.99, 0, "El Aceite de Almendras dulces es básico para una hidratación y nutrición de la piel. Puedes utilizarlo en todas las partes de tu cuerpo preferiblemente después de la ducha con la piel húmeda, para ayudar a su absorción llegando a las capas profundas de la piel. Un aceite neutro apto para todo tipo de pieles y edades, utilizándose para toda la familia desde las edades más tempranas. Es ideal como base para formularlo con otros aceites, aceites esenciales y lociones.", "../img/12.jpg")
+var p13 = new producto(1, "Aceite esencial de Clavo", "12ML", 8, 665, "El aceite esencial de clavo es conocido por sus increíbles propiedades antimicrobianas, antimicóticas, antisépticas, antivirales, afrodisíacas y estimulantes. Perfecto para utilizar en tus mezclas de Cosmética Natural, añadiendo unas cuantas gotas en tu crema corporal o aceite vegetal, conseguirás nutrir y lucir una piel radiante y 100% cuidado.", "../img/1.jpg")
 data_principal.push(p1)
 data_principal.push(p2)
 data_principal.push(p3)
@@ -25,47 +26,38 @@ data_principal.push(p9)
 data_principal.push(p10)
 data_principal.push(p11)
 data_principal.push(p12)
+data_principal.push(p13)
 
 
-function main() {
-    for (var i = 0; i < data_principal.length; i++) {
-        var contenedor = document.getElementById("contenedor")
-        var plantilla = `<div class="card carta">
-            <svg class="corazon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                    <path
-                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                </svg>
-            <img class="card-img-top imagen" src="${data_principal[i].imagen}" alt="Card image cap">
-            <h5 class="texto_titulo">${data_principal[i].nombre }</h5>
-            <div class="card-body">
-                <p class="complemento">${data_principal[i].detalle}</p>
-                <p class="complemento_money">$${data_principal[i].precio}</p>
-            </div>
-            <button class="boton"><svg style=" margin-bottom: 2.5%; margin-right: 5%;"
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-basket3-fill" viewBox="0 0 16 16">
-                        <path
-                            d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
-                    </svg>Añadir a la cesta</button>
-            </div>`
+// FUNCIONES PIE DE PAGINA 
 
-        contenedor.innerHTML += plantilla;
-    }
-
+function lengs() {
+    var index = Math.ceil((data_principal.length) / 12)
+    return index
 }
 
-function busqueda(palabra) {
+function index_s() {
+    var index = lengs()
+    var contendor = document.getElementById("contenedor_pagina")
+    for (var p = 1; p < index + 1; p++) {
+        var plantilla_index = `<li class="page-item paginado"><a class="page-link texto_paginado" href="#">${p}</a></li>`
+        contendor.innerHTML += plantilla_index
+    }
+}
+
+//FUNCIONES PRINCIPALES
+
+function main() {
     var contenedor = document.getElementById("contenedor")
-    contenedor.innerHTML = ""
-    for (var i = 0; i < data_principal.length; i++) {
-        if (data_principal[i].nombre.toLowerCase().includes(palabra.toLowerCase())) {
+    if (data_principal.length <= 12) {
+        for (var i = 0; i < data_principal.length; i++) {
             var plantilla = `<div class="card carta">
             <svg class="corazon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                     <path
                         d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                 </svg>
             <img class="card-img-top imagen" src="${data_principal[i].imagen}" alt="Card image cap">
-            <h5 class="texto_titulo">${data_principal[i].nombre }</h5>
+            <h5 class="texto_titulo">${data_principal[i].nombre}</h5>
             <div class="card-body">
                 <p class="complemento">${data_principal[i].detalle}</p>
                 <p class="complemento_money">$${data_principal[i].precio}</p>
@@ -77,10 +69,113 @@ function busqueda(palabra) {
                             d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
                     </svg>Añadir a la cesta</button>
             </div>`
-            contenedor.innerHTML += plantilla
+            contenedor.innerHTML += plantilla;
+
+        }
+    } else {
+        for (var i = 0; i < 12; i++) {
+            var plantilla = `<div class="card carta">
+            <svg class="corazon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                    <path
+                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                </svg>
+            <img class="card-img-top imagen" src="${data_principal[i].imagen}" alt="Card image cap">
+            <h5 class="texto_titulo">${data_principal[i].nombre}</h5>
+            <div class="card-body">
+                <p class="complemento">${data_principal[i].detalle}</p>
+                <p class="complemento_money">$${data_principal[i].precio}</p>
+            </div>
+            <button class="boton"><svg style=" margin-bottom: 2.5%; margin-right: 5%;"
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-basket3-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
+                    </svg>Añadir a la cesta</button>
+            </div>`
+            contenedor.innerHTML += plantilla;
+
         }
     }
 
+    var contendor = document.getElementById("contenedor_pagina")
+    contendor.innerHTML = ""
+    index_s()
 }
+
+//NAVEGACIÓN 
+
+document.getElementById("xd").addEventListener("click", (e) => {
+    var index = e.target.innerHTML
+    var contenedor = document.getElementById("contenedor")
+    contenedor.innerHTML = ""
+    if (index == 1) {
+        main()
+    } else if (index > 1) {
+        var regla = 12
+        var final = (index * regla) - regla
+        for (var i = final; i < data_principal.length; i++) {
+            var plantilla = `<div class="card carta">
+                <svg class="corazon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                        <path
+                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
+                <img class="card-img-top imagen" src="${data_principal[i].imagen}" alt="Card image cap">
+                <h5 class="texto_titulo">${data_principal[i].nombre}</h5>
+                <div class="card-body">
+                    <p class="complemento">${data_principal[i].detalle}</p>
+                    <p class="complemento_money">$${data_principal[i].precio}</p>
+                </div>
+                <button class="boton"><svg style=" margin-bottom: 2.5%; margin-right: 5%;"
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-basket3-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
+                        </svg>Añadir a la cesta</button>
+                </div>`
+            contenedor.innerHTML += plantilla;
+        }
+    }
+    var contendor = document.getElementById("contenedor_pagina")
+    contendor.innerHTML = ""
+    index_s()
+})
+
+//BUSQUEDA
+document.getElementById("filtro_string").addEventListener("click", (e) => {
+    var contenedor = document.getElementById("contenedor")
+    var palabra_str = document.getElementById("busqueda_str").value.toLowerCase()
+    if (palabra_str != "") {
+        contenedor.innerHTML = ""
+        for (var i = 0; i < data_principal.length; i++) {
+            if (data_principal[i].nombre.toLowerCase().includes(palabra_str)) {
+                var plantilla = `<div class="card carta">
+            <svg class="corazon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                    <path
+                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                </svg>
+            <img class="card-img-top imagen" src="${data_principal[i].imagen}" alt="Card image cap">
+            <h5 class="texto_titulo">${data_principal[i].nombre}</h5>
+            <div class="card-body">
+                <p class="complemento">${data_principal[i].detalle}</p>
+                <p class="complemento_money">$${data_principal[i].precio}</p>
+            </div>
+            <button class="boton"><svg style=" margin-bottom: 2.5%; margin-right: 5%;"
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-basket3-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
+                    </svg>Añadir a la cesta</button>
+            </div>`
+                contenedor.innerHTML += plantilla
+            }
+        }
+    } else {
+        alert("Digite algo")
+    }
+})
+
+//MODALES
+
+
+//EJECUCIÓN
 main()
-busqueda("parches")
