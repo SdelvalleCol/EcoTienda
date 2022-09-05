0b00debc880e36cc624596304f091904e5651e53
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2022-09-04 23:15:31 -0500

    Save

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -368,3 +368,44 @@ function añadir(id) {
         }
     }
 }
+
+//Modal_Carrito_FullScreen
+document.getElementById("ir_carrito_fs").addEventListener("click", (e) => {
+    var contenedor = document.getElementById("compras_fs_id")
+    var dinero = 0
+    for (var p = 0; p < data_carrito.length; p++) {
+        var plantilla = `<div style="width:100% ;">
+        <div class="row">
+            <div class="col-3">
+                <img src="${data_carrito[p].imagen}" alt="" style="width: 100%;">
+            </div>
+            <div class="col-5">
+                <p style="font-weight: bold; font-family:sans-serif; word-wrap: break-word;">${data_carrito[p].nombre}</p>
+                <p style="margin-top: -5%; font-family:sans-serif; font-size: 80%; color: rgb(175, 175, 175);">${data_carrito[p].detalle}</p>
+            </div>
+            <div class="col-2">
+                <input min="1" placeholder="Cantidad" type="text" style="width: 100%; text-align: center;">
+            </div>
+            <div class="col-1">
+                $4
+            </div>
+            <div class="col-1">
+                <svg id="boton_borrar_fullscren_carrito" style="width: 100%; align-items: center;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
+                    <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z"/>
+                  </svg>
+            </div>
+        </div>
+    </div>`
+        dinero += parseFloat(data_carrito[p].precio)
+        contenedor.innerHTML += plantilla
+    }
+    document.getElementById("compras_fs_subtotal").innerHTML = dinero + "$"
+    document.getElementById("compras_fs_total_s").innerHTML = dinero + (dinero * 0.19) + "$"
+    document.getElementById("modal_fs_c").style.display = "flex"
+    document.getElementById("carrito_compras").style.display = "none"
+})
+
+function reverse() {
+    document.getElementById("modal_fs_c").style.display = "none"
+    document.getElementById("carrito_compras").style.display = "flex"
+}
